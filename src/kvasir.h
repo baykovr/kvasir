dc61f645fc857638b6b7d1c1b8b4881539a237e8
Robert Baykov <baykovr@gmail.com>
2017-01-06 20:06:30 -0600

    Cleanup makefile, prep for refactor.

--- src/kvasir.h
+++ src/kvasir.h
@@ -10,4 +10,4 @@ int task_rails(char* rails[], int num_rails);
 int dispence_rail(char* rail[],int rail_size,const char* payload);
 int proc_scheduler(char* host_file_path);
 
-const char* ACTIVE_MSG = "YOUR GET REQUEST\r\n\r\n";
+const char* ACTIVE_MSG = "GET index.html HTTP1.0\r\n\r\n";
